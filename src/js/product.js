2ffceb63472597eed926a61cf27b2ab3840da7bc
ellachamb <ellachamb@gmail.com>
2024-01-26 15:10:35 -0700

    productDetails2

--- src/js/product.js
+++ src/js/product.js
@@ -1,34 +1,5 @@
 import { getParam } from "./utils.mjs";
-import renderProductDetails from "./productDetails.mjs"; 
+import renderProductDetails from "./productDetails.mjs";
 
-const productId = getParam("product"); 
-renderProductDetails(productId, ".product-detail"); 
-
-//MOVED TO productDetails.mjs
-
-// const dataSource = new ProductData("tents");
-
-// function addProductToCart(product) {
-//   addItemToStorageArray("so-cart", product);
-// }
-
-// add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
-
-
-// const myProduct = new ProductData("tents"); 
-// async function getProduct() {
-//   const currentProduct = await myProduct.findProductById(productId); 
-//   console.log(currentProduct); 
-// }
-// getProduct(); 
-
-
+const productId = getParam("product");
+renderProductDetails(productId, ".product-detail");
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,35 +1,29 @@
 import { addItemToStorageArray } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
-const myProduct = new ProductData("tents"); 
+const myProduct = new ProductData("tents");
 
-// async function addToCartHandler(e) {
-//     const product = await myProduct.findProductById(e.target.dataset.id);
-//     addProductToCart(product);
-// }  
-
-let product = {}; 
+let product = {};
 
 export default async function renderProductDetails(productId, selector) {
-    product = await myProduct.findProductById(productId); 
-    console.log(product);
-    const el = document.querySelector(selector); 
-    el.innerHTML = productDetailsTemplate(product); 
-    document.getElementById("addToCart").addEventListener("click", ()=> addToCart(product));
-    
-    // const currentProduct = await myProduct.findProductById(productId); 
-    // document.querySelector(selector).innerHTML = productDetailsTemplate(currentProduct); 
-    // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  product = await myProduct.findProductById(productId);
+  const el = document.querySelector(selector);
+  el.innerHTML = productDetailsTemplate(product);
+  document
+    .getElementById("addToCart")
+    .addEventListener("click", () => addToCart(product));
+
+  // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
   // once we have the product details we can render out the HTML
   // add a listener to Add to Cart button
 }
 
 export function addToCart(product) {
-    addItemToStorageArray("so-cart", product);
+  addItemToStorageArray("so-cart", product);
 }
 
 function productDetailsTemplate(product) {
-    return `<h3>${product.Brand.Name}</h3>
+  return `<h3>${product.Brand.Name}</h3>
   <h2 class="divider">${product.NameWithoutBrand}</h2>
   <img
     class="divider"
