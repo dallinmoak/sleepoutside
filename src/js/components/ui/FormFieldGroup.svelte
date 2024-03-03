b12fb68e21f809d35c8f50c279c338ce91a354c6
dallinmoak <dallinmoak@gmail.com>
2024-03-02 17:09:39 -0700

    format & style order form read only fields

--- src/js/components/ui/FormFieldGroup.svelte
+++ src/js/components/ui/FormFieldGroup.svelte
@@ -6,10 +6,10 @@
 <fieldset>
   <legend>{legend}</legend>
   {#each fields as field}
-    <div>
+    <div class={field.readOnly ? "readonly" : ""}>
       <label for={field.id}>{field.label}</label>
       {#if field.readOnly}
-        <span id={field.id} readonly>{field.value}</span>
+        <span id={field.id}>{field.value}</span>
       {:else}
         <input
           type={field.type}
@@ -44,5 +44,15 @@
     width: 100%;
     padding: 0.5em;
     border: 1px solid var(--light-grey);
+  }
+
+  div.readonly {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  div:has(span#orderTotal) {
+    border-top: 1px solid black;
+    font-weight: 900;
   }
 </style>
